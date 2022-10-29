de4fb57f725ea94aae24b8c29b550e1c818294cd
听风 <55818117+iosyyy@users.noreply.github.com>
2022-10-29 11:14:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ MD_HEAD = """
 <p align="center">
 <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Noto+Sans+Simplified+Chinese&size=25&pause=1000&color=%23F75C7E&center=true&vCenter=true&width=435&lines=%E9%A1%B9%E7%9B%AE%E5%80%9F%E9%89%B4%E8%87%AA+Gitblog+%E6%84%9F%E8%B0%A2%E5%A4%A7%E4%BD%AC%E7%9A%84%E9%A1%B9%E7%9B%AE%E2%99%A5;%E6%9C%AC%E9%A1%B9%E7%9B%AE%E6%98%AFiosyyy%E7%9A%84%E7%A7%81%E4%BA%BA%E5%8D%9A%E5%AE%A2;%E5%8F%AF%E9%9A%8F%E6%84%8Fcv%E6%B2%A1%E6%9C%89%E7%89%88%E6%9D%83;%E6%9C%AC%E4%BA%BA%E6%98%AF%E4%B8%AA%E8%8F%9C%E9%B8%A1+%E5%A6%82%E6%9C%89%E9%94%99%E8%AF%AF%E6%84%9F%E8%B0%A2%E5%A4%A7%E4%BD%AC%E6%8C%87%E6%AD%A3" alt="Typing SVG" /></a>
 </p>
-[The RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 
+<div align="center">
+
+[![HitCount](http://hits.dwyl.com/iosyyy/issuses-blog.svg)](https://github.com/iosyyy/issuses-blog)
+<a href="https://github.com/Meghna-DAS/github-profile-views-counter">
+    <img src="https://komarev.com/ghpvc/?username=iosyyy">
+</a>
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat-square)](https://github.com/iosyyy/issuses-blog/blob/main/WELCOME.md#user-content-welcome)
+<a href="https://github.com/iosyyy?tab=followers"><img src="https://img.shields.io/github/followers/iosyyy?label=Followers&style=social" alt="GitHub Badge"></a>
+<!-- Docs badge not working ... if you have time to help investigate, please do.
+[![Inline docs](http://inch-ci.org/github/dwyl/hits.svg?style=flat-square)](http://inch-ci.org/github/dwyl/hits)
+-->
+</div>
 """
 
 BACKUP_DIR = "BACKUP"
